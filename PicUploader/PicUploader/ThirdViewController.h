ba90b51a4c22704b29dc83bcec5f2efdb9396307
james.gan <ganzhi@gmail.com>
2011-12-03 21:29:14 +1300

    Add a third view

---
+++ PicUploader/PicUploader/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  PicUploader
+//
+//  Created by James Gan on 12/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
